9062e307d0a3cb40707dc6e8edf55ca342e50001
Victor Martinez <victor.martinez@migcan.com>
2011-11-05 21:47:23 +0000

    Implemented time as a gen_widget

--- apps/widgy/src/widgy_sup.erl
+++ apps/widgy/src/widgy_sup.erl
@@ -10,7 +10,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(WIDGET(Type, Frequency), {Type, {gen_widget, start_link, [Type, Frequency]}, permanent, 5000, worker, [gen_widget]}).
 
 %% ===================================================================
 %% API functions
@@ -25,21 +25,25 @@ start_link() ->
 
 init([]) ->
     {ok, Port} = application:get_env(widgy, websockets_port),
+
     WidgyWebsocketsServer = {widgy_websockets_server,
                              {widgy_websockets_server, start_link, [Port]},
-                             permanent, 5000, worker, [widgy_websockets_server]},
+                              permanent, 5000, worker, [widgy_websockets_server]},
 
     WidgySubscriptionsHandler = {widgy_subscriptions_handler,
                              {widgy_subscriptions_handler, start_link, []},
                              permanent, 5000, worker, [widgy_subscriptions_handler]},
 
-    WidgySubscriptionsHandler = {widgy_data_gatherer,
+    WidgyDataGatherer = {widgy_data_gatherer,
                              {widgy_data_gatherer, start_link, []},
                              permanent, 5000, worker, [widgy_data_gatherer]},
 
-    TimeServer = {time,
-                  {time, start_link, []},
-                  permanent, 5000, worker, [time]},
+    TimeServer = ?WIDGET(time, 10000),
+    TemperatureServer = ?WIDGET(temperature, 3000),
 
-    {ok, { {one_for_one, 5, 10}, [WidgyWebsocketsServer, WidgySubscriptionsHandler, TimeServer]} }.
+    {ok, { {one_for_one, 5, 10},
+           [WidgyWebsocketsServer, WidgySubscriptionsHandler, WidgyDataGatherer, TimeServer,
+           TemperatureServer]
+         }
+    }.
 
