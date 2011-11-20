f32fd97ecf93b373b9abb14972699e422a5cc3e3
Victor Martinez <victor.martinez@migcan.com>
2011-11-20 15:04:18 +0000

    Handle call with list / pid params

--- apps/widgy/src/widgy_counter.erl
+++ apps/widgy/src/widgy_counter.erl
@@ -6,12 +6,17 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--export([get_count/1, increment/1]).
+-export([get_state/1, increment/1]).
 
 -record(state, {count=0}).
 
-get_count(Pid) ->
-    gen_server:call(Pid, get_count).
+get_state(CounterId) when is_list(CounterId) ->
+    Pid = widgy:get_widget_pid(CounterId),
+    gen_server:call(Pid, get_state).
+
+increment(CounterId) when is_list(CounterId) ->
+    Pid = widgy:get_widget_pid(CounterId),
+    increment(Pid);
 
 increment(Pid) ->
     gen_server:call(Pid, increment).
@@ -29,6 +34,9 @@ handle_call(get_count, _From, State) ->
 handle_call(increment, _From, State) ->
     NewCount = State#state.count + 1,
     {reply, NewCount, State#state{count=NewCount}};
+
+handle_call(get_state, _From, State) ->
+    {reply, [{count, State#state.count}], State};
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
