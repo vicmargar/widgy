927851ba85585b7d2406623ea75bceff0eed4a08
Victor Martinez <victor.martinez@migcan.com>
2011-11-20 15:03:47 +0000

    create the widget and dashboard tables in the _app

--- apps/widgy/src/widgy_app.erl
+++ apps/widgy/src/widgy_app.erl
@@ -2,15 +2,35 @@
 
 -behaviour(application).
 
-%% Application callbacks
 -export([start/2, stop/1]).
+
+-export([setup/0]).
+
+-include_lib("widgy/include/widgy.hrl").
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    ?WIDGETS_TABLE = ets:new(?WIDGETS_TABLE, [named_table, public, set]),
+    ?DASHBOARDS_TABLE = ets:new(?DASHBOARDS_TABLE, [named_table, public, set]),
+
     widgy_sup:start_link().
+
 
 stop(_State) ->
     ok.
+
+
+setup() ->
+    DashboardId = widgy:create_dashboard(20000),
+
+    Counter1 = widgy:start_counter(),
+    Counter2 = widgy:start_counter(),
+    Counter3 = widgy:start_counter(),
+
+    widgy:add_widget_to_dashboard(Counter1, DashboardId),
+    widgy:add_widget_to_dashboard(Counter2, DashboardId),
+    widgy:add_widget_to_dashboard(Counter3, DashboardId).
+
